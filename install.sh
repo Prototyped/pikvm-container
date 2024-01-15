320aa56eb999646404580063d5299833065bcb6a
Amit Gurdasani <gurdasani@gmail.com>
2024-01-15 11:29:20 +0000

    Set up dwc2 and libcomposite modules to be loaded at boot.

--- install.sh
+++ install.sh
@@ -9,6 +9,11 @@ then
 fi
 
 mkdir -p /var/lib/kvmd/msd /var/lib/kvmd/pst /var/log/kvmd/nginx
+install -oroot -groot -m0644 kvmd.conf /etc/modules-load.d/kvmd.conf
 install -oroot -groot -m0644 pikvm-container.service /etc/systemd/system/
 systemctl daemon-reload
+while read -r mod
+do
+    modprobe $mod
+done < kvmd.conf
 systemctl enable --now pikvm-container
